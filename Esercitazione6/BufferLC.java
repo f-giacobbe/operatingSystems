652c7ba294e41cdfa3d2cad7c274fabe1deed03b
Francesco Giacobbe <francescogiacob11@live.it>
2025-05-12 19:43:18 +0200

    Prima parte Esercitazione 6 - Lock e Condition

---
+++ Esercitazione6/BufferLC.java
@@ -0,0 +1,63 @@
+package Esercitazione6;
+
+import Esercitazione5.Buffer;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class BufferLC extends Buffer {
+    protected final int[] buffer;
+    protected int in = 0;
+    protected int out = 0;
+    protected int numElementi = 0;
+    protected Lock l = new ReentrantLock();
+    protected Condition bufferVuoto = l.newCondition();
+    protected Condition bufferPieno = l.newCondition();
+
+    public BufferLC(int dimensione) {
+        buffer = new int[dimensione];
+    }
+
+    @Override
+    public void put(int i) {
+        l.lock();
+        try {
+            while (numElementi == buffer.length) {
+                bufferPieno.await();
+            }
+
+            buffer[in] = i;
+            in = (in + 1) % buffer.length;
+            numElementi++;
+            bufferVuoto.signal();
+        } catch (InterruptedException _) {
+
+        } finally {
+            l.unlock();
+        }
+    }
+
+    @Override
+    public int get() {
+        int i;
+        l.lock();
+        try {
+            while (numElementi == 0) {
+                bufferVuoto.await();
+            }
+
+            i = buffer[out];
+            out = (out + 1) % buffer.length;
+            numElementi--;
+            bufferPieno.signal();
+            return i;
+        } catch (InterruptedException _) {
+
+        } finally {
+            l.unlock();
+        }
+
+        throw new IllegalStateException();
+    }
+}
---
+++ Esercitazione6/ContoCorrenteLC.java
@@ -0,0 +1,34 @@
+package Esercitazione6;
+
+import Esercitazione3.ContoCorrente;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ContoCorrenteLC extends ContoCorrente {
+    private final Lock l = new ReentrantLock();
+
+    public ContoCorrenteLC(int deposito) {
+        super(deposito);
+    }
+
+    @Override
+    public void deposita(int importo) {
+        l.lock();
+        try {
+            deposito += importo;
+        } finally {
+            l.unlock();
+        }
+    }
+
+    @Override
+    public void preleva(int importo) {
+        l.lock();
+        try {
+            deposito -= importo;
+        } finally {
+            l.unlock();
+        }
+    }
+}
